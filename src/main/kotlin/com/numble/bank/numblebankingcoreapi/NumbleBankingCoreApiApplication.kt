8f47aa456cef9a7c761496902b4bddf8b62ae67e
devbloo <zpswl45@naver.com>
2023-02-18 15:15:38 +0900

    init: project

---
+++ src/main/kotlin/com/numble/bank/numblebankingcoreapi/NumbleBankingCoreApiApplication.kt
@@ -0,0 +1,11 @@
+package com.numble.bank.numblebankingcoreapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NumbleBankingCoreApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<NumbleBankingCoreApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/numble/bank/numblebankingcoreapi/NumbleBankingCoreApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.numble.bank.numblebankingcoreapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NumbleBankingCoreApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
